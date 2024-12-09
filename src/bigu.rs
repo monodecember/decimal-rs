8d6d387e06c424cb7887f9afe7589a8f355640e3
monodecember <166819823+monodecember@users.noreply.github.com>
2024-12-09 05:23:14 +0000

    add BigU::from

--- src/bigu.rs
+++ src/bigu.rs
@@ -1,8 +1,25 @@
+// 주의! 이 모듈은 decimal-rs 에서 작동하도록 만들어졌으며, 일반적인 사용에 적합하지 않음.
+
 pub struct BigU {
-    v: Vec<u32>,
-    sign: bool
+    num: Vec<u32>,
 }
 
-pub trait ToBigU {
-    fn to_biguint(&self) -> Option<BigU>;
+impl BigU {
+    fn from(s: String) -> Self {
+        let mut num: Vec<u32> = Vec::new();
+
+        match s.parse::<u32>() {
+            Ok(ok_num) => num.push(ok_num),
+            Err(e) => match e.kind() {
+                    std::num::IntErrorKind::PosOverflow => { num = to_bigu(s); },
+                    _ => panic!("Not a valid integer."),
+            },
+        }
+
+        Self { num }
+    } // or 모든 정수를 u8 으로 표현하기. 3213 = [3, 2, 1, 3] 자리수 계산이 편한 대신 느릴 것.
+}
+
+fn to_bigu(s: String) -> Vec<u32> {
+    Vec::new()
 }
